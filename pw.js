2786f2e047a352ffeeb3c4d741ca735be8c6b369
Phil Thompson <phil.thompson@digital.justice.gov.uk>
2014-10-19 12:32:37 +0100

    wip reimplemented with callbacks - now to complete testing

--- pw.js
+++ pw.js
@@ -1,51 +1,230 @@
 #!/usr/bin/env node
 'use strict';
 
-var fs = require('fs'),
-    path = require('path'),
+// pw
+//
+// Script for storing and retreiving account credentials encrypted using PGP.
+// Store website, username/email and password.
+// Places password into clipboard for easy pasting into website or app.
+//
+// Tested on Linux, Windows and Mac.
+//
+// Only dependency is node (No node modules are required).
+//
+// Find pw and other useful scripts at https://github.com/PhilT/bin
+//
+// version 0.8 - Convert to Node.js
+// version 0.7 - Fixed incorrect use of extended bash if
+//               Added description
+// version 0.6 - Silence some verbose commands
+// version 0.5 - Add support for OSX
+//               Overwrite existing passwords (--force)
+//               15 character passwords including symbols
+//               Optional safe characters (--safe)
+// version 0.4 - Add support for Git Bash aka MinGW
+// version 0.3 - generate password and copy to clipboard
+//               Don't re-encrypt file if not changed
+//               Copy retrieved password to clipboard
+//               Improve display of multiple passwords
+// version 0.2 - Added --edit command
+// version 0.1 - Not much error handling yet.
+//
+// Add to ~/bin/pw and chmod +x it.
+// See bottom of file for usage or run `pw` without args.
+//
+
+var path = require('path'),
+    exec = require('child_process').exec,
+    fs = require('fs'),
     readline = require('readline').createInterface({
       input: process.stdin,
       output: process.stdout
     }),
-    exec = require('child_process').exec,
+    safe = false,
+    params = [],
+    force = false,
+    passwordChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+    homePath = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE),
+    waitingToExit = 0,
     commands = {},
+    attemptExit,
+    copyPassword,
+    enterPassword,
+    generatePassword,
+    gpg,
+    loadConfig,
+    loadFile,
+    loadJson,
+    loadPasswordFile,
     parseArgs,
     puts,
-    readPassword,
-    usage;
+    savePasswordFile,
+    start,
+    usage,
+    wantToExit;
 
-// process.stdin.resume();
-// process.on('SIGINT', function () {
-//   process.exit(0);
-// });
+process.stdin.resume();
+process.on('SIGINT', function () {
+  process.exit(0);
+});
 
-commands.init = function init() {
-  var password;
-  password = readPassword();
-  puts(password);
+attemptExit = function attemptExit() {
+  waitingToExit -= 1;
+  if (waitingToExit === 0) {
+    process.exit(1);
+  }
 };
 
-commands.edit = function edit() {
+commands.generate = function generate(passwords, params, pwconfig) {
+  var url = params[0],
+      login = params[1],
+      siteMatched = passwords.indexOf(url) !== -1,
+      password = generatePassword(pwconfig.passwordLength),
+      line = [url, login, password + "\n"].join(','),
+      newPasswords;
 
+  if (!siteMatched) {
+    if (passwords[length.passwords - 1] !== "\n") { passwords += "\n"; }
+    newPasswords = passwords + line;
+    copyPassword(password);
+  } else if (force) {
+    newPasswords = passwords.replace(new RegExp(url + ",.*,.*\n"), line);
+    copyPassword(password);
+  } else {
+    puts('Password for %s already exists. Use --force to overwrite', url);
+  }
+
+  return newPasswords;
 };
 
-commands.generate = function generate() {
-
+commands.list = function list(passwords) {
+  puts(passwords);
+  return passwords;
 };
 
-commands.query = function query() {
+commands.query = function query(passwords, params) {
+  var term = params[0],
+      regexp = new RegExp(".*" + term + ".*\n", 'g'),
+      matches,
+      parts;
+  matches = passwords.match(regexp);
+  if (matches.length === 0) {
+    puts('No matches.');
+  } else if (matches === 1) {
+    parts = matches[0].split(',').replace("\n", '');
+    copyPassword(parts[0], parts[1], parts[2]);
+  } else {
+    puts('Multiple passwords matched. Displaying:');
+    puts(matches.join(''));
+  }
 
+  return passwords;
+};
+
+commands.remove = function remove(passwords, params, pwconfig) {
+  var newPasswords = passwords.slice();
+  return newPasswords;
+};
+
+copyPassword = function copyPassword(url, login, password) {
+  var noNewline = '-n ',
+      command;
+
+  if (os.platform() === 'win32') {
+    noNewline = '|set /p=';
+    command = 'clip';
+  } else if (os.platform() === 'darwin') {
+    command = 'pbcopy';
+  } else {
+    command = 'xsel -i';
+  }
+
+  wantToExit();
+  exec(util.format("echo %s%s|%s", noNewline, password, command), function () {
+    puts('Password for %s copied to clipboard. Login: %s', url, login);
+    attemptExit();
+  });
+};
+
+enterPassword = function enterPassword(callback) {
+  readline.question('Enter encryption password:', function (answer) {
+    readline.close();
+    callback(answer);
+  });
+};
+
+generatePassword = function generatePassword(length) {
+  var password = '',
+      max = passwordChars.length,
+      i;
+
+  for (i = 0; i < length; i += 1) {
+    password += passwordChars[Math.floor(Math.random() * max)];
+  }
+  return password;
+};
+
+gpg = function gpg(password, encrypt, input, callback) {
+  var options = [
+      '--symetric',
+      '--no-permission-warning',
+      '--no-secmem-warning',
+      '--force-mdc',
+      '--no-tty',
+      '-q',
+      '--no-use-agent',
+      '--yes',
+      '--passphrase=' + password,
+      '--homedir ' + path.join(homePath, '.gnupg')
+    ],
+    child;
+
+  if (encrypt) { options.push('--encrypt'); }
+  child = exec('gpg', options, callback);
+  child.stdin.write(input, function () {
+    child.stdin.end();
+  });
+};
+
+loadConfig = function loadConfig(pathname) {
+  var config = loadJson(pathname);
+  config.passwordLength = config.passwordLength || 15;
+
+  if (!config.dir || !config.file || !config.editor) {
+    puts(pathname + ' must exist with, for example:');
+    puts('{"dir": "path/to/password/file", "file": ".password.csv"}');
+    process.exit(1);
+  }
+  return config;
+};
+
+loadFile = function loadFile(pathname) {
+  var contents = null;
+  try {
+    contents = fs.readFileSync(pathname, 'utf8');
+  } catch (e) {}
+  return contents;
+};
+
+loadJson = function loadJson(pathname) {
+  return JSON.parse(loadFile(pathname)) || {};
+};
+
+loadPasswordFile = function loadPasswordFile(pathname, password, callback) {
+  var encryptedPasswords = loadFile(pathname);
+  if (!encryptedPasswords) { return callback(''); }
+  gpg(password, false, encryptedPasswords, function (passwords) {
+    callback(passwords);
+  });
 };
 
 parseArgs = function parseArgs() {
   var args = process.argv.slice(2),
       command = args.shift(),
-      validCommands = ['init', 'edit', 'generate', 'query', 'i', 'e', 'g', 'q'],
-      index = validCommands.indexOf(command),
-      safe = false,
-      params = [],
-      force = false,
-      passwordChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      commands = ['delete', 'generate', 'list', 'query'],
+      index = commands.indexOf(command);
+
+  commands = commands.concat(commands.map(function (command) { return command[0]; }));
 
   if (index === -1) {
     usage();
@@ -68,36 +247,66 @@ parseArgs = function parseArgs() {
     passwordChars += '_!@#$%^*()\\-+=';
   }
 
-  commands[validCommands[index % 4]]();
+  return commands[index % 4];
 };
 
 puts = function puts() {
   console.log.apply(this, arguments);
 };
 
-readPassword = function readPassword(callback) {
-  readline.question('Enter encryption password:', function (answer) {
-    readline.close();
-    callback(answer);
+savePasswordFile = function savePasswordFile(filepath, password, passwords) {
+  wantToExit();
+  gpg(password, true, passwords, function (encryptedPasswords) {
+    fs.writeFileSync(filepath, encryptedPasswords);
+    puts('Password file updated.');
+    attemptExit();
+  });
+};
+
+start = function start() {
+  var command = parseArgs(),
+      pwconfigPath = path.join(homePath, '.pwconfig.json'),
+      pwconfig = loadConfig(pwconfigPath),
+      passwordFile = path.join(pwconfig.dir, pwconfig.file) + '.gpg';
+
+  enterPassword(function (password) {
+    loadPasswordFile(passwordFile, password, function (passwords) {
+      var newPasswords = commands[command](passwords, params, pwconfig);
+      if (newPasswords !== passwords) {
+        savePasswordFile(passwordFile, password, passwords);
+      } else {
+        puts('No changes made.');
+      }
+    });
   });
 };
 
 usage = function usage() {
   puts([
     'pw - generate, store and retrieve encrypted passwords',
+    'install: npm install promise',
     'usage: pw command options',
     '',
     ' command:',
-    '   i, init                 encrypts a new passwords file',
-    '   e, edit                 open passwords file in editor',
-    '   g, generate url login   create a new password',
-    '   q, query term           find a password',
+    '   g, generate url login   creates a new password',
+    '   l, list                 displays all passwords',
+    '   q, query term           finds a password',
+    '   r, remove site          removes a password, site and login',
     '',
     ' options:',
     '   --force   overwrite existing password',
     '   --safe    use only alphanumeric characters (no symbols)',
+    '',
+    ' notes:',
+    '   query will copy a password to clipboard if a single match is found.',
+    '     otherwise it displays all matches',
+    '   delete will only remove a password where the site matches exactly',
     ''
   ].join("\n"));
 };
 
-parseArgs();
+wantToExit = function wantToExit() {
+  waitingToExit += 1;
+};
+
+start();
--- pw_to_js.js
+++ pw_to_js.js
@@ -37,192 +37,192 @@
     // path = require('path'),
     // readline = require('readline'),
     // exec = require('child_process').exec,
-    paths = {},
-    config = {},
+    // paths = {},
+    // config = {},
     // rli;
 
-function cleanup() {
-  fs.unlinkSync(paths.temp);
-  fs.unlinkSync(paths.tempBak);
-}
+// function cleanup() {
+//   fs.unlinkSync(paths.temp);
+//   fs.unlinkSync(paths.tempBak);
+// }
 
-function execSync(command) {
-  exec(command, function (error, stdout, stderr) {
-    if (error !== null) {
-      console.log("Error %d running '%s'.", error.code, command);
-      console.log(stderr);
-      process.exit(1);
-    }
-  });
-}
+// function execSync(command) {
+//   exec(command, function (error, stdout, stderr) {
+//     if (error !== null) {
+//       console.log("Error %d running '%s'.", error.code, command);
+//       console.log(stderr);
+//       process.exit(1);
+//     }
+//   });
+// }
 
-function encrypt() {
-  var options = (config.options + ['-c', '-o', paths.gpg, paths.temp]).join(' ');
-  execSync('gpg ' + options);
-  cleanup();
-  console.log('Password file updated.');
-}
+// function encrypt() {
+//   var options = (config.options + ['-o', paths.gpg, paths.temp]).join(' ');
+//   execSync('gpg ' + options);
+//   cleanup();
+//   console.log('Password file updated.');
+// }
 
-function decrypt() {
-  var options = (config.options + ['-o', paths.temp, paths.gpg]).join(' ');
-  execSync('gpg ' + options);
-}
+// function decrypt() {
+//   var options = (config.options + ['-o', paths.temp, paths.gpg]).join(' ');
+//   execSync('gpg ' + options);
+// }
 
-function backupDecruptedFile() {
-  fs.createReadStream(paths.temp).pipe(fs.createWriteStream(paths.tempBak));
-}
+// function backupDecruptedFile() {
+//   fs.createReadStream(paths.temp).pipe(fs.createWriteStream(paths.tempBak));
+// }
 
-function initPasswordCsv() {
-  encrypt();
-}
+// function initPasswordCsv() {
+//   encrypt();
+// }
 
-function editPasswordCsv() {
-  decrypt();
-  backupDecruptedFile();
-  exec(util.format("%s %s", config.editor, paths.temp), function (error) {
-    if (error !== null) {
-      exec(util.format("diff %s %s", paths.temp, paths.tempBak), function (error) {
-        if (error !== null) {
-          if (error.code === 1) {
-            encrypt();
-          } else {
-            console.log(util.format("Error diffing %s with %s", paths.temp, paths.tempBak));
-            cleanup();
-            process.exit(1);
-          }
-        } else {
-          cleanup();
-          console.log('No changes made.');
-        }
-      });
-    }
-  });
-}
+// function editPasswordCsv() {
+//   decrypt();
+//   backupDecruptedFile();
+//   exec(util.format("%s %s", config.editor, paths.temp), function (error) {
+//     if (error !== null) {
+//       exec(util.format("diff %s %s", paths.temp, paths.tempBak), function (error) {
+//         if (error !== null) {
+//           if (error.code === 1) {
+//             encrypt();
+//           } else {
+//             console.log(util.format("Error diffing %s with %s", paths.temp, paths.tempBak));
+//             cleanup();
+//             process.exit(1);
+//           }
+//         } else {
+//           cleanup();
+//           console.log('No changes made.');
+//         }
+//       });
+//     }
+//   });
+// }
 
-function generatePassword() {
-  var password = '',
-      max = config.chars.length,
-      i = 0;
-  for (; i < config.passwordLength; i += 1) {
-    password += config.chars[Math.floor(Math.random() * max)];
-  }
-  return password;
-}
+// function generatePassword() {
+//   var password = '',
+//       max = config.chars.length,
+//       i = 0;
+//   for (; i < config.passwordLength; i += 1) {
+//     password += config.chars[Math.floor(Math.random() * max)];
+//   }
+//   return password;
+// }
 
-function copyPassword(password) {
-  if (os.platform() === 'win32') {
-    exec('echo ' + password + ' | clip');
-  } else if (os.platform() === 'darwin') {
-    exec('echo -n ' + password + ' | pbcopy');
-  } else {
-    exec('echo -n ' + password + ' | xsel -i');
-  }
-}
+// function copyPassword(password) {
+//   if (os.platform() === 'win32') {
+//     exec('echo ' + password + ' | clip');
+//   } else if (os.platform() === 'darwin') {
+//     exec('echo -n ' + password + ' | pbcopy');
+//   } else {
+//     exec('echo -n ' + password + ' | xsel -i');
+//   }
+// }
 
-function loadPasswords() {
-  return fs.readFileSync(paths.temp, {encoding: 'UTF-8'});
-}
+// function loadPasswords() {
+//   return fs.readFileSync(paths.temp, {encoding: 'UTF-8'});
+// }
 
-function addPassword(site, login) {
-  var siteMatched,
-      passwords,
-      password,
-      newPassword;
-  decrypt();
-  passwords = loadPasswords();
-  siteMatched = passwords.indexOf(site) !== -1;
-  newPassword = [site, login, password + "\n"].join(',');
+// function addPassword(site, login) {
+  // var siteMatched,
+      // passwords,
+      // password,
+      // newPassword;
+  // decrypt();
+  // passwords = loadPasswords();
+  // siteMatched = passwords.indexOf(site) !== -1;
+  // newPassword = [site, login, password + "\n"].join(',');
 
-  if (!siteMatched || argv.force) {
-    password = generatePassword();
-    copyPassword(password);
-    console.log('Password copied to clipboard.');
+  // if (!siteMatched || argv.force) {
+    // password = generatePassword();
+    // copyPassword(password);
+    // console.log('Password copied to clipboard.');
 
-    if (!siteMatched) {
-      if (passwords[length.passwords - 1] !== "\n") { passwords += "\n"; }
-      passwords += newPassword;
-    } else {
-      passwords.replace(new RegExp(site + ",.*,.*\n"), newPassword);
-    }
-    encrypt();
-  } else {
-    cleanup();
-    console.log('Password for %s already exists. Use --force to overwrite', site);
-  }
-}
+    // if (!siteMatched) {
+    //   if (passwords[length.passwords - 1] !== "\n") { passwords += "\n"; }
+    //   passwords += newPassword;
+    // } else {
+    //   passwords.replace(new RegExp(site + ",.*,.*\n"), newPassword);
+    // }
+    // encrypt();
+  // } else {
+    // cleanup();
+    // console.log('Password for %s already exists. Use --force to overwrite', site);
+//   }
+// }
 
-function findPassword(site) {
-  var regexp = new RegExp(".*" + site + ".*\n", 'g'),
-      passwords,
-      matches,
-      parts;
-  decrypt();
-  passwords = loadPasswords();
-  matches = passwords.match(regexp);
-  if (matches.length === 0) {
-    console.log('No matches.');
-  } else if (matches === 1) {
-    parts = matches[0].split(',').replace("\n", '');
-    copyPassword(parts[2]);
-    console.log('Password for %s copied to clipboard. Login: %s', parts[0], parts[1]);
-  } else {
-    console.log('Multiple passwords matched. Displaying:');
-    console.log(matches.join(''));
-  }
-  cleanup();
-}
+// function findPassword(site) {
+//   var regexp = new RegExp(".*" + site + ".*\n", 'g'),
+//       passwords,
+//       matches,
+//       parts;
+//   decrypt();
+//   passwords = loadPasswords();
+//   matches = passwords.match(regexp);
+//   if (matches.length === 0) {
+//     console.log('No matches.');
+//   } else if (matches === 1) {
+//     parts = matches[0].split(',').replace("\n", '');
+//     copyPassword(parts[2]);
+//     console.log('Password for %s copied to clipboard. Login: %s', parts[0], parts[1]);
+//   } else {
+//     console.log('Multiple passwords matched. Displaying:');
+//     console.log(matches.join(''));
+//   }
+//   cleanup();
+// }
 
-paths.home = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE);
-paths.config = path.join(paths.home, '.pwconfig.json');
+// paths.home = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE);
+// paths.config = path.join(paths.home, '.pwconfig.json');
 
-try {
-  config = JSON.parse(fs.readFileSync(paths.config, {encoding: 'UTF-8'}));
-  paths.dir = config.dir;
-  paths.file = paths.file;
-} catch (e) {}
+// try {
+//   config = JSON.parse(fs.readFileSync(paths.config, {encoding: 'UTF-8'}));
+//   paths.dir = config.dir;
+//   paths.file = paths.file;
+// } catch (e) {}
+//
+// if (!paths.dir || !paths.file || !config.editor) {
+//   console.log(paths.config + ' must exist. For example:');
+//   console.log('{"dir": "password/dir", "file": ".password.csv", "editor": "vim"}');
+//   process.exit(1);
+// }
 
-if (!paths.dir || !paths.file || !config.editor) {
-  console.log(paths.config + ' must exist. For example:');
-  console.log('{"dir": "password/dir", "file": ".password.csv", "editor": "vim"}');
-  process.exit(1);
-}
-
-config.passwordLength = 15;
-paths.gpg = path.join(config.dir, paths.file) + '.gpg';
-paths.temp = path.join(os.tmpdir(), paths.file);
-paths.tempBak = paths.temp + '.bak';
-
-if (!fs.existsSync(config.dir)) {
-  console.log(config.dir + ' does not exist. Check ' + paths.config);
-  process.exit(1);
-}
-
-if (!fs.existsSync(paths.gpg)) {
-  console.log(paths.gpg + ' does not exist. Check ' + paths.config);
-  process.exit(1);
-}
+// config.passwordLength = 15;
+// paths.gpg = path.join(config.dir, paths.file) + '.gpg';
+// paths.temp = path.join(os.tmpdir(), paths.file);
+// paths.tempBak = paths.temp + '.bak';
+//
+// if (!fs.existsSync(config.dir)) {
+//   console.log(config.dir + ' does not exist. Check ' + paths.config);
+//   process.exit(1);
+// }
+//
+// if (!fs.existsSync(paths.gpg)) {
+//   console.log(paths.gpg + ' does not exist. Check ' + paths.config);
+//   process.exit(1);
+// }
 
 // rli = readline.createInterface({
 //   input: process.stdin,
 //   output: process.stdout
 // });
 
-if (argv.size > 0) {
-  rli.question('Enter encryption password:', function (answer) {
-    rli.close();
-    config.options = [
-      '--no-permission-warning',
-      '--no-secmem-warning',
-      '--force-mdc',
-      '--no-tty',
-      '-q',
-      '--no-use-agent',
-      '--yes',
-      '--passphrase=' + answer,
-      '--homedir ' + path.join(path.home, '.gnupg')
-    ];
-  });
-}
+// if (argv.size > 0) {
+//   rli.question('Enter encryption password:', function (answer) {
+//     rli.close();
+//     config.options = [
+//       '--no-permission-warning',
+//       '--no-secmem-warning',
+//       '--force-mdc',
+//       '--no-tty',
+//       '-q',
+//       '--no-use-agent',
+//       '--yes',
+//       '--passphrase=' + answer,
+//       '--homedir ' + path.join(path.home, '.gnupg')
+//     ];
+//   });
+// }
 
 // config.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 //
